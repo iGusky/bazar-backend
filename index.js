6329e1591747cb76973bbba8d52879753e78a173
iGusky <gustavohdezmorales@gmail.com>
2021-06-03 10:27:48 -0500

    puerto agregado

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 4000
 
 app.listen( port, host, () => {
-  console.log('Servidor funcionando en el puerto 4000');
+  console.log('Servidor funcionando en el puerto' + port);
 })
