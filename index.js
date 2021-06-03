7d556f15398d8f81e9b70dc77460599e732205a4
iGusky <gustavohdezmorales@gmail.com>
2021-06-03 10:47:30 -0500

    mensaje para depurar

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 4000
 
 app.listen(port, host, () => {
-  console.log('Servidor funcionando en el puerto' + port);
+  console.log('Servidor funcionando en el puerto ' + port + ' host ' + host);
 })
